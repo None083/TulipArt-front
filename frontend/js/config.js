5b1842946fc730fa98834973ef6dadbd8b9f53e0
Noelia <perezgonzaleznoelia08@gmail.com>
2025-06-09 15:32:30 +0200

    cambio url servicios php

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,7 +2,7 @@
 const appConfig = {
     apiBaseUrl: window.location.hostname === 'localhost'
         ? 'http://localhost/TulipArt/backend/servicios_rest_protect'
-        : '/backend/servicios_rest_protect',
+        : 'https://tulipart-production.up.railway.app/backend/servicios_rest_protect',
     getApiUrl: function(endpoint) {
         return this.apiBaseUrl + endpoint;
     }
